8749746ceb5b52d01306e73b6118c0f389a0a85a
jjuidev <jjuidev@gmail.com>
2026-09-07 22:18:05 +0700

    feat: add Java Spring CLI

---
+++ apps/java-spring-leetcode/src/main/java/com/jjuidev/jsl/JavaSpringLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.jjuidev.jsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringLeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringLeetcodeApplication.class, args);
+	}
+
+}
---
+++ apps/java-spring-leetcode/src/test/java/com/jjuidev/jsl/JavaSpringLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjuidev.jsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
